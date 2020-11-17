4dfd6767590da472abfc642f5bd27c90411ac13a
baswanth-oruganti <baswanth.oruganti@gmail.com>
2020-11-17 09:10:59 +0100

    pdf

--- normal-dist.py
+++ normal-dist.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+mu = 0.3
+sigma = 0.1
 
 np.random.seed(123)
-data = np.random.normal(0.3,0.01,100000)
-plt.hist(data, bins=25, range=(0.25,0.35))
+data = np.random.normal(mu,sigma,100000)
+count, bins, ignored = plt.hist(data, bins=25, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+
+         linewidth=2, color='r')
 plt.show()
