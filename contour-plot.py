1ef299e3effcddaa6c89702352d1989e22564d77
baswanth-oruganti <baswanth.oruganti@gmail.com>
2020-12-08 18:50:53 +0100

    updated

--- contour-plot.py
+++ contour-plot.py
@@ -8,14 +8,23 @@ from scipy import interpolate
 import matplotlib.mlab as ml
 import matplotlib.tri as tri
 import matplotlib
+import pandas as pd
 
-data = np.loadtxt(sys.argv[1])
+
+data=pd.read_csv("fes_60.dat",header=None,sep="\s+",usecols=[i for i in range(3)])
+data.columns = ["s[R]","z[R]", "DG"]
+#data = np.loadtxt(sys.argv[1])
 
 
-x = data[:, 0]
-y = data[:, 1]
+x = data["s[R]"]
+y = data["z[R]"]
+z = data["DG"]
 
-z = data[:, 2]
+print(x)
+
+#x = data[:, 0]
+#y = data[:, 1]
+#z = data[:, 2]
 
 breaks = np.linspace(0, 12, 13)
 
@@ -36,7 +45,7 @@ plt.ylabel('CV2 (degrees)')
 #plt.legend()
 #plt.grid()
 
-plt.axis([0, 180, 0, 180]) # [xstart, xend, ystart, yend]
+plt.axis([-3.1, 3.1, -3.1, 3.1]) # [xstart, xend, ystart, yend]
 plt.show()
 
 
